5cbb0b3b022715757ca891affbf2ae60f9471cf6
OnyeEbube <okontaonyeka@gmail.com>
2023-07-05 11:58:27 +0000

    Adding file 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A module that holds a lockbox function
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    A function that unlocks boxes
+    """
+    unlocked = set([0])
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        keys = boxes[box]
+
+        for key in keys:
+            if key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                stack.append(key)
+    
+    return len(unlocked) == len(boxes)
