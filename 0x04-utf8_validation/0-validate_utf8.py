6a81c31d71bda4ec0fd60f2f5d4537b812c7bcbf
OnyeEbube <okontaonyeka@gmail.com>
2023-07-25 20:27:56 +0000

    Adding solution file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,12 @@
 def validUTF8(data):
     """Checks for a valid UTF-8 encoding
     """
+    for num in data:
+        if num not in range(256):
+            return False
     try:
-        data.decode('utf-8')
+        data_bytes = bytes(data)
+        data_bytes.decode('utf-8')
         return True
     except UnicodeDecodeError:
         return False
